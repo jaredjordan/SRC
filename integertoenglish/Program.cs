b2ceede6451bffe1073b3223375ee059ae9d7cee
jaredjordan <jaredjordan@live.com>
2017-02-28 13:24:37 -0800

    Initial check-in of module integertoenglish

---
+++ integertoenglish/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+//First Handle if the number is negative or positive 
+//then break down from left to right 
+
+namespace integertoenglish
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var numbertoenglish = 11;
+			var tensnteens = new List<string> {"zero", "one", "two", "three","four","five","six","seven","eight","nine","ten","eleven" };
+			var endings = new List <string> {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+			var bignumbers = new List<string> {"hundred","thousand","million","billion" };
+			var neg = new List<string> { "negative"};
+
+			if(numbertoenglish < 20) 
+				
+				Console.WriteLine(tensnteens[numbertoenglish]);
+		}
+	}
+}
